b45978d4b780b9f5a91aa82f57115e10db11de17
Dongyun Xue 4090 <xuedongyunxdy@163.com>
2023-11-28 10:45:02 +0800

    change local.sh

--- run_local_train.sh
+++ run_local_train.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python main.py --batch_size=2 --grad_accum_steps=4 --max_steps=20000 --val_steps=500 --lr=1e-3
+python main.py --batch_size=2 --grad_accum_steps=16 --max_steps=20000 --val_steps=500 --lr=1e-3
