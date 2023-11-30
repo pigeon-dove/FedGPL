2417abf125ff1287a8a1f0ddde33d69fb1a81583
Dongyun Xue <xuedongyunxdy@163.com>
2023-11-30 11:49:51 +0800

    change eval file csv logic

--- eval_ans_acc.py
+++ eval_ans_acc.py
@@ -22,8 +22,8 @@ token = "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"
 model_name = "meta-llama/Llama-2-7b-chat-hf"
 data_name = "gsm8k"
 
-exp_name = "231128-2116"
-weights_file = "weights-250"
+exp_name = "gsm8k-origin"
+# weights_file = "weights-250"
 seed = 3407
 batch_size = 2
 instruction = "Please solve the following math problem and provide the answer in the format '### <number>' at the end."
@@ -43,8 +43,8 @@ tokenizer.sep_token = tokenizer.eos_token
 tokenizer.cls_token = tokenizer.eos_token
 tokenizer.mask_token = tokenizer.eos_token
 
-model = PeftModel.from_pretrained(model, f"./result/{exp_name}/{weights_file}", device_map="auto")
-model = model.merge_and_unload()
+# model = PeftModel.from_pretrained(model, f"./result/{exp_name}/{weights_file}", device_map="auto")
+# model = model.merge_and_unload()
 model.eval()
 
 set_seed(seed)
@@ -56,7 +56,7 @@ total_size = len(full_dataset)
 train_size = int(0.6 * total_size)
 val_size = int(0.2 * total_size)
 test_size = total_size - train_size - val_size
-train_ds, val_ds, test_ds = random_split(full_dataset, [train_size, val_size, test_size])
+_, _, test_ds = random_split(full_dataset, [train_size, val_size, test_size])
 
 
 # %%
@@ -79,9 +79,11 @@ def get_correct_ans(s):
 
 
 # %%
-exp_dict = {"question": [], "answer": [], "output": [], "correct": [], "output_num": [], "acc": []}
+csv_path = f"./result/{exp_name}/eval_ans_acc.csv"
 loop = tqdm(test_ds, desc="test", position=0)
 for data in loop:
+    new_row = {"question": None, "answer": None, "output": None, "correct": None, "output_num": None, "acc": None}
+
     question = data["question"]
     prompt = f"[INST] <<SYS>>\n{instruction}\n<</SYS>>\n{question} [/INST]"
     input = tokenizer(prompt, return_tensors="pt").to(model.device)
@@ -90,7 +92,7 @@ for data in loop:
 
     generate_ids = model.generate(
         input_ids=input['input_ids'],
-        max_new_tokens=512,
+        max_new_tokens=1024,
         do_sample=False,
         top_p=1,
         temperature=1,
@@ -101,17 +103,17 @@ for data in loop:
     output_num = get_correct_ans(output)
     acc = 1 if correct == output_num else 0
 
-    exp_dict["question"].append(question)
-    exp_dict["answer"].append(answer)
-    exp_dict["output"].append(output)
-    exp_dict["correct"].append(correct)
-    exp_dict["output_num"].append(output_num)
-    exp_dict["acc"].append(acc)
+    new_row["question"] = question
+    new_row["answer"] = answer
+    new_row["output"] = output
+    new_row["correct"] = correct
+    new_row["output_num"] = output_num
+    new_row["acc"] = acc
 
     loop.set_postfix(correct=correct,
                      output_num=output_num,
-                     acc=acc,
-                     mean_acc=sum(exp_dict["acc"]) / len(exp_dict["acc"]))
+                     acc=acc)
+    # mean_acc=sum(exp_dict["acc"]) / len(exp_dict["acc"])
 
-
-pd.DataFrame(exp_dict).to_csv(f"./result/{exp_name}/eval_ans_acc.csv", index=True)
+    df = pd.DataFrame([new_row])
+    df.to_csv(csv_path, mode='a', header=not pd.read_csv(csv_path).shape[0])
