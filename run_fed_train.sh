aadfeffcaea454247a118f8f15425b77fe4bb2fa
Dongyun Xue <xuedongyunxdy@163.com>
2023-12-20 12:26:54 +0800

    layer 8:0

--- run_fed_train.sh
+++ run_fed_train.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python main.py --device="cuda:0" --train_mode="fedSplit" --client_num=50 --client_num_per_step=4 --client_epoch=1 --batch_size=2 --client_batch_per_step=8 --grad_accum_steps=4 --max_steps=4000 --val_steps=50 --lr=5e-4 --client_lr=1e-4
+python main.py --device="cuda:0" --train_mode="fedSplit" --client_num=50 --client_num_per_step=4 --client_epoch=1 --batch_size=2 --client_batch_per_step=8 --grad_accum_steps=4 --max_steps=1000 --val_steps=50 --lr=5e-4 --client_lr=1e-4
